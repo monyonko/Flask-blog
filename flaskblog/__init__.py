1878af04bec9ba8ac8e95d48e73edc41263d4fb7
monyonko <monyonko.miriam@gmail.com>
2020-08-20 23:27:58 +0300

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+
+bcrypt= Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view= 'login'
+login_manager.login_message_category = 'info'
+
+
+mail = Mail(app)
+
+from flaskblog.users.routes import users
+app.register_blueprint(users)
+
+from flaskblog.posts.routes import posts
+app.register_blueprint(posts)
+
+from flaskblog.main.routes import main
+app.register_blueprint(main)
---
+++ flaskblog/users/forms.py
@@ -0,0 +1,65 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_login import current_user
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), 
+    Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password =PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    submit= SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username exists. Please choose another one")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email exists. Please choose another one")
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password =PasswordField('Password', validators=[DataRequired()])
+    remember=BooleanField('Remember Me')
+    submit= SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), 
+    Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png','PNG'])])
+    submit= SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username exists. Please choose another one")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email exists. Please choose another one")
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("That email is not registered. You must register first")
+
+class ResetPasswordForm(FlaskForm):
+    password =PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    submit= SubmitField('Reset Password')
